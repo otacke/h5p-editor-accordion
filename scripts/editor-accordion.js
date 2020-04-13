c051ce50775863ba8069696e426f5c8c201834f7
Oliver Tacke <o.tacke@posteo.de>
2020-04-13 18:00:01 +0200

    Remove Accordions from child Column when pasted

--- scripts/editor-accordion.js
+++ scripts/editor-accordion.js
@@ -8,18 +8,97 @@ H5PEditor.Accordion = (function () {
     // Outsource readies
     that.passReadies = true;
 
-    // No particular DOM for widget, just used to override Column's library options
-    that.libraryAccordion = new H5PEditor.widgets[field.type](parent, field, params, setValue);
+    /**
+     * Sanitize Column options.
+     */
+    const sanitizeColumnOptions = function () {
+      // H5P.Column editor list needs to be loaded
+      if (!that.libraryAccordion || !that.libraryAccordion.children || that.libraryAccordion.children.length === 0) {
+        return;
+      }
 
-    // Remove Accordion as option from subcontent
-    that.libraryAccordion.change(function () {
-      // H5P.Column editor list (children[0]) has been loaded
+      // Remove Accordion from Column options
       that.libraryAccordion.children[0].forEachChild(function (child) {
         const libraryColumn = H5PEditor.findField('content', child);
         libraryColumn.field.options = libraryColumn.field.options.filter(function (option) {
           return option.indexOf('H5P.Accordion ') === -1;
         });
       });
+    };
+
+    /**
+     * Find ids of Accordion instances in Column.
+     * @return {number[]} Ids of Accordion instances in Column.
+     */
+    const findAccordionIds = function () {
+      if (!that.libraryAccordion || !that.libraryAccordion.params || !that.libraryAccordion.params.params || !that.libraryAccordion.params.params.content) {
+        return;
+      }
+
+      const columnContents = that.libraryAccordion.params.params.content;
+
+      // Get ids of Accordions inside Column
+      const columnAccordionIds = [];
+      for (let i = columnContents.length - 1; i >= 0; i--) {
+        if (columnContents[i].content && columnContents[i].content.library && columnContents[i].content.library.indexOf('H5P.Accordion ') !== -1) {
+          columnAccordionIds.push(i);
+        }
+      }
+
+      return columnAccordionIds;
+    };
+
+    /**
+     * Sanitize Column parameters.
+     * @param {number[]} columnAccordionIds Ids of Accordion instances in Column.
+     */
+    const sanitizeColumnParams = function (columnAccordionIds) {
+      if (!columnAccordionIds) {
+        return;
+      }
+
+      if (!that.libraryAccordion || !that.libraryAccordion.params || !that.libraryAccordion.params.params || !that.libraryAccordion.params.params.content) {
+        return;
+      }
+
+      // Remove Accordions from params
+      that.libraryAccordion.params.params.content = that.libraryAccordion.params.params.content.filter(function (content, index) {
+        return columnAccordionIds.indexOf(index) === -1;
+      });
+    };
+
+    /**
+     * Sanitize Column DOM in editor.
+     * @param {number[]} columnAccordionIds Ids of Accordion instances in Column.
+     */
+    const sanitizeColumnDOM = function (columnAccordionIds) {
+      if (!columnAccordionIds) {
+        return;
+      }
+
+      // Remove Accordions from editor DOM
+      const columnContentsDOM = that.libraryAccordion.$libraryWrapper.get(0).querySelector('.list > .h5peditor-widget-wrapper > ul');
+      if (!columnContentsDOM.childNodes) {
+        return;
+      }
+
+      columnAccordionIds.forEach(function (id) {
+        that.libraryAccordion.children[0].removeItem(id);
+        if (columnContentsDOM.childNodes.length > id) {
+          columnContentsDOM.removeChild(columnContentsDOM.childNodes[id]);
+        }
+      });
+    };
+
+    // No particular DOM for widget, just used to override Column's library options
+    that.libraryAccordion = new H5PEditor.widgets[field.type](parent, field, params, setValue);
+
+    // Remove Accordion as option from subcontent
+    that.libraryAccordion.change(function () {
+      const columnAccordionIds = findAccordionIds();
+      sanitizeColumnParams(columnAccordionIds);
+      sanitizeColumnDOM(columnAccordionIds);
+      sanitizeColumnOptions();
     });
 
     /**
